a536ca2ca674a8ba7160405553279d8ee7740f8c
Redia-pdf <2511500027@mahasiswa.atmaluhur.ac.id>
2026-09-18 17:27:47 +0700

    test

---
+++ mahasiswa.php
@@ -0,0 +1,13 @@
+{
+    "status": "success",
+    "message": "Data mahasiswa berhasil diambil",
+    "data": {
+        "id": 1,
+        "nim": "202401001",
+        "nama": "Budi Santoso",
+        "email": "budi@example.com",
+        "jurusan": "Teknik Informatika",
+        "semester": 5,
+        "ipk": 3.75
+    }
+}
